867add81fb1a8e129020bad4547279aaba08e8d8
Lukáš Neubauer <lukasneubauer@seznam.cz>
2015-10-09 09:39:41 +0200

    User entity

---
+++ app/model/Entities/UserEntity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class UserEntity extends BaseEntity
+{
+    /**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue
+     *
+     * @var int
+     */
+    protected $id;
+
+	/**
+	 * @ORM\Column(type="string", unique=true, nullable=true)
+	 *
+	 * @var string
+	 */
+	protected $username;
+
+	/**
+	 * @ORM\Column(type="string", unique=true, nullable=true)
+	 *
+	 * @var string
+	 */
+	protected $email;
+
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 *
+	 * @var string
+	 */
+	protected $forename;
+
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 *
+	 * @var string
+	 */
+	protected $surname;
+}
