11333c4c13d8b8c39f26cf688358f6f6de92d054
Lukáš Neubauer <lukasneubauer@seznam.cz>
2015-12-24 22:27:17 +0100

    Templates, config files and misc

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -3,44 +3,34 @@
 namespace App\Presenters;
 
 use Nette;
-use App\Model;
 use Tracy\ILogger;
 
-
-class ErrorPresenter extends BasePresenter
+class ErrorPresenter extends Nette\Object implements Nette\Application\IPresenter
 {
-	/** @var ILogger */
-	private $logger;
+    /** @var ILogger */
+    private $logger;
 
+    public function __construct(ILogger $logger)
+    {
+        $this->logger = $logger;
+    }
 
-	public function __construct(ILogger $logger)
-	{
-		$this->logger = $logger;
-	}
+    /**
+     * @return Nette\Application\IResponse
+     */
+    public function run(Nette\Application\Request $request)
+    {
+        $e = $request->getParameter('exception');
 
+        if ($e instanceof Nette\Application\BadRequestException) {
+            // $this->logger->log("HTTP code {$e->getCode()}: {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", 'access');
+            return new Nette\Application\Responses\ForwardResponse($request->setPresenterName('Error4xx'));
+        }
 
-	/**
-	 * @param  Exception
-	 * @return void
-	 */
-	public function renderDefault($exception)
-	{
-		if ($exception instanceof Nette\Application\BadRequestException) {
-			$code = $exception->getCode();
-			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
-			// log to access.log
-			$this->logger->log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
+        $this->logger->log($e, ILogger::EXCEPTION);
 
-		} else {
-			$this->setView('500'); // load template 500.latte
-			$this->logger->log($exception, ILogger::EXCEPTION); // and log exception
-		}
-
-		if ($this->isAjax()) { // AJAX request? Note this error in payload.
-			$this->payload->error = TRUE;
-			$this->terminate();
-		}
-	}
-
+        return new Nette\Application\Responses\CallbackResponse(function () {
+            require __DIR__ . '/templates/Error/500.phtml';
+        });
+    }
 }
