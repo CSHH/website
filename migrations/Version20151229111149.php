ab85420535b9d30f338b97ddda6a296198b51e11
Lukáš Neubauer <lukasneubauer@seznam.cz>
2015-12-29 11:34:25 +0100

    DB migration

---
+++ migrations/Version20151229111149.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151229111149 extends AbstractMigration
+{
+	/**
+	 * @param Schema $schema
+	 */
+	public function up(Schema $schema)
+	{
+		// this up() migration is auto-generated, please modify it to your needs
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+		$this->addSql('ALTER TABLE user ADD token VARCHAR(255) DEFAULT NULL AFTER role, ADD token_created_at DATETIME DEFAULT NULL AFTER token, ADD is_authenticated TINYINT(1) NOT NULL AFTER token_created_at');
+	}
+
+	/**
+	 * @param Schema $schema
+	 */
+	public function down(Schema $schema)
+	{
+		// this down() migration is auto-generated, please modify it to your needs
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+		$this->addSql('ALTER TABLE `user` DROP token, DROP token_created_at, DROP is_authenticated');
+	}
+}
