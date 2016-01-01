86d485a0007a056b8c6f7638be05f30fe00449fe
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-01 18:17:07 +0100

    User section

--- app/presenters/UserSectionPresenter.php
+++ app/presenters/UserSectionPresenter.php
@@ -38,6 +38,54 @@ final class UserSectionPresenter extends SecurePresenter
         $this->template->items = $this->items;
     }
 
+    public function actionVideos()
+    {
+        $items = $this->videoCrud->getAllByUserForPage($this->page, 10, $this->getLoggedUser());
+        $this->preparePaginator($items->count(), 10);
+        $this->items = $items;
+    }
+
+    public function renderVideos()
+    {
+        $this->template->items = $this->items;
+    }
+
+    public function actionGames()
+    {
+        $items = $this->wikiCrud->getAllByUserForPage($this->page, 10, $this->getLoggedUser(), Entities\WikiEntity::TYPE_GAME);
+        $this->preparePaginator($items->count(), 10);
+        $this->items = $items;
+    }
+
+    public function renderGames()
+    {
+        $this->template->items = $this->items;
+    }
+
+    public function actionMoview()
+    {
+        $items = $this->wikiCrud->getAllByUserForPage($this->page, 10, $this->getLoggedUser(), Entities\WikiEntity::TYPE_MOVIE);
+        $this->preparePaginator($items->count(), 10);
+        $this->items = $items;
+    }
+
+    public function renderMovies()
+    {
+        $this->template->items = $this->items;
+    }
+
+    public function actionBooks()
+    {
+        $items = $this->wikiCrud->getAllByUserForPage($this->page, 10, $this->getLoggedUser(), Entities\WikiEntity::TYPE_BOOK);
+        $this->preparePaginator($items->count(), 10);
+        $this->items = $items;
+    }
+
+    public function renderBooks()
+    {
+        $this->template->items = $this->items;
+    }
+
     /**
      * @return Entities\UserEntity
      */
