11b89332a0b81f8ee6160d7f2522547d02f9f139
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-09 19:10:10 +0100

    Articles - activation and deleting links

--- app/FrontModule/presenters/ArticlePresenter.php
+++ app/FrontModule/presenters/ArticlePresenter.php
@@ -50,4 +50,38 @@ final class ArticlePresenter extends SingleUserContentPresenter
     {
         $this->template->article = $this->article;
     }
+
+    /**
+     * @param int $articleId
+     */
+    public function handleActivate($articleId)
+    {
+        $article = $articleId ? $this->articleRepository->getById($articleId) : null;
+
+        if (!$article) {
+            $this->throw404();
+        }
+
+        $this->articleRepository->activate($article);
+
+        $this->flashMessage($this->translator->translate('locale.item.activated'));
+        $this->redirect('this');
+    }
+
+    /**
+     * @param int $articleId
+     */
+    public function handleDelete($articleId)
+    {
+        $article = $articleId ? $this->articleRepository->getById($articleId) : null;
+
+        if (!$article) {
+            $this->throw404();
+        }
+
+        $this->articleRepository->delete($article);
+
+        $this->flashMessage($this->translator->translate('locale.item.deleted'));
+        $this->redirect('this');
+    }
 }
