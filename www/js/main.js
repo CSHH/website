3b1756d35b31b1461edaffd79f1c3e01f92b2406
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-09 19:07:35 +0100

    JS

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function() {
+
+    (function() {
+        $('[data-confirm]').click(function(e) {
+            var q = $(this).attr('data-confirm');
+            var a = window.confirm(q);
+            if (a === false) {
+                e.preventDefault();
+            }
+        });
+    })();
 
 });
