cf7db1e2884351df25752580c27485ada6633c6d
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-09 12:36:38 +0100

    User settings - presenter and template

---
+++ app/AdminModule/presenters/SettingsPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use App\AdminModule\Components\Forms;
+use App\Model\Security\Authenticator;
+
+final class SettingsPresenter extends SecurePresenter
+{
+    /**
+     * @return Forms\ProfileSettingsForm
+     */
+    protected function createComponentProfileSettingsForm()
+    {
+        return new Forms\ProfileSettingsForm(
+            $this->translator,
+            $this->userRepository,
+            new Authenticator($this->translator, $this->userRepository),
+            $this->getUser()->getStorage(),
+            $this->getLoggedUser()
+        );
+    }
+}
