21faa9d4118da880d3c1c0cd442e052bfa2e1e22
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-10 22:28:17 +0100

    WikiForm - create draft

--- app/AdminModule/components/forms/WikiForm.php
+++ app/AdminModule/components/forms/WikiForm.php
@@ -8,11 +8,15 @@ use App\Model\Entities;
 use App\Model\Exceptions;
 use Nette\Application\UI\Form;
 use Nette\Localization\ITranslator;
+use Nette\Utils\DateTime;
 
 class WikiForm extends AbstractContentForm
 {
     /** @var Repositories\WikiRepository */
     private $wikiRepository;
+
+    /** @var Repositories\WikiDraftRepository */
+    private $wikiDraftRepository;
 
     /** @var string */
     private $type;
@@ -24,6 +28,7 @@ class WikiForm extends AbstractContentForm
      * @param ITranslator $translator
      * @param Repositories\TagRepository $tagRepository
      * @param Repositories\WikiRepository $wikiRepository
+     * @param Repositories\WikiDraftRepository $wikiDraftRepository
      * @param Entities\UserEntity $user
      * @param string $type
      * @param Entities\WikiEntity $item
@@ -32,6 +37,7 @@ class WikiForm extends AbstractContentForm
         ITranslator $translator,
         Repositories\TagRepository $tagRepository,
         Repositories\WikiRepository $wikiRepository,
+        Repositories\WikiDraftRepository $wikiDraftRepository,
         Entities\UserEntity $user,
         $type,
         Entities\WikiEntity $item = null
@@ -39,6 +45,7 @@ class WikiForm extends AbstractContentForm
         parent::__construct($translator, $tagRepository, $user);
 
         $this->wikiRepository = $wikiRepository;
+        $this->wikiDraftRepository = $wikiDraftRepository;
         $this->type     = $type;
         $this->item     = $item;
     }
@@ -53,6 +60,8 @@ class WikiForm extends AbstractContentForm
 
         $form->addTextArea('text', 'locale.form.text')
             ->setRequired('locale.form.text_required');
+
+        $form->addHidden('startTime', date('Y-m-d H:i:s'));
     }
 
     public function formSucceeded(Form $form)
@@ -63,17 +72,38 @@ class WikiForm extends AbstractContentForm
             $tag    = $this->getSelectedTag($form);
 
             if ($this->item) {
+
                 if ($this->item->isActive) {
-                    // save draft
-                    dump('draft');exit;
+
+                    $latest = $this->wikiDraftRepository->getLatestByWiki($this->item);
+                    $start  = DateTime::from($values->startTime);
+
+                    if ($latest && $start < $latest->createdAt) {
+                        throw new Exceptions\WikiDraftConflictException(
+                            $this->translator->translate('locale.error.newer_draft_created_meanwhile')
+                        );
+                    }
+
+                    unset($values->name);
+                    unset($values->perex);
+                    unset($values->startTime);
+
+                    $this->wikiDraftRepository->create($values, $this->user, $this->item, new Entities\WikiDraftEntity);
+                    $ent = $this->item;
+                    $p->flashMessage($this->translator->translate('locale.item.updated'));
+
                 } else {
                     $ent = $this->wikiRepository->update($values, $tag, $this->user, $this->type, $this->item);
                     $p->flashMessage($this->translator->translate('locale.item.updated'));
                 }
+
             } else {
                 $ent = $this->wikiRepository->create($values, $tag, $this->type, new Entities\WikiEntity);
                 $p->flashMessage($this->translator->translate('locale.item.created'));
             }
+
+        } catch (Exceptions\WikiDraftConflictException $e) {
+            $this->addFormError($form, $e);
 
         } catch (Exceptions\MissingTagException $e) {
             $this->addFormError($form, $e);
