820738515710e8e4baa200d8e5decf01919336fb
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-01-11 20:46:09 +0100

    Wiki-draft form in book and movie sections

--- app/FrontModule/presenters/BookPresenter.php
+++ app/FrontModule/presenters/BookPresenter.php
@@ -2,6 +2,7 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\FrontModule\Components\Forms;
 use App\Model\Entities;
 
 final class BookPresenter extends SharedContentPresenter
@@ -12,5 +13,21 @@ final class BookPresenter extends SharedContentPresenter
     public function actionDefault($tagSlug)
     {
         $this->runActionDefault($tagSlug, 10, Entities\WikiEntity::TYPE_BOOK);
+    }
+
+    /**
+     * @return Forms\WikiDraftForm
+     */
+    protected function createComponentForm()
+    {
+        return new Forms\WikiDraftForm(
+            $this->translator,
+            $this->tagRepository,
+            $this->wikiRepository,
+            $this->wikiDraftRepository,
+            $this->getLoggedUser(),
+            Entities\WikiEntity::TYPE_BOOK,
+            $this->wiki
+        );
     }
 }
--- app/FrontModule/presenters/MoviePresenter.php
+++ app/FrontModule/presenters/MoviePresenter.php
@@ -2,6 +2,7 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\FrontModule\Components\Forms;
 use App\Model\Entities;
 
 final class MoviePresenter extends SharedContentPresenter
@@ -12,5 +13,21 @@ final class MoviePresenter extends SharedContentPresenter
     public function actionDefault($tagSlug)
     {
         $this->runActionDefault($tagSlug, 10, Entities\WikiEntity::TYPE_MOVIE);
+    }
+
+    /**
+     * @return Forms\WikiDraftForm
+     */
+    protected function createComponentForm()
+    {
+        return new Forms\WikiDraftForm(
+            $this->translator,
+            $this->tagRepository,
+            $this->wikiRepository,
+            $this->wikiDraftRepository,
+            $this->getLoggedUser(),
+            Entities\WikiEntity::TYPE_MOVIE,
+            $this->wiki
+        );
     }
 }
