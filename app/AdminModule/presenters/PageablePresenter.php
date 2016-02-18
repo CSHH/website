9f3f97dd8cfdac44dbe61f5346f824455a5224ef
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-02-18 17:01:09 +0100

    Using ActivityTrait

--- app/AdminModule/presenters/PageablePresenter.php
+++ app/AdminModule/presenters/PageablePresenter.php
@@ -3,24 +3,20 @@
 namespace App\AdminModule\Presenters;
 
 use App\Model\Entities;
+use App\Presenters\ActivityTrait;
 use App\Presenters\PageableTrait;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 abstract class PageablePresenter extends SecurePresenter
 {
+    use ActivityTrait;
     use PageableTrait;
-
-    /** @var string @persistent */
-    public $inactiveOnly = 'no';
 
     /** @var Paginator */
     protected $items;
 
     /** @var Entities\BaseEntity */
     protected $item;
-
-    /** @var bool */
-    protected $displayInactiveOnly = false;
 
     /** @var bool */
     protected $canAccess = false;
@@ -30,12 +26,5 @@ abstract class PageablePresenter extends SecurePresenter
         $this->template->inactiveOnly = $this->displayInactiveOnly;
         $this->template->canAccess    = $this->canAccess;
         $this->template->items        = $this->items;
-    }
-
-    protected function checkIfDisplayInactiveOnly()
-    {
-        if ($this->inactiveOnly === 'yes') {
-            $this->displayInactiveOnly = true;
-        }
     }
 }
