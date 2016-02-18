9ba110097c8510292e06240fbf4d878e8bf4f8dd
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-02-18 16:28:59 +0100

    Added PageableTrait

---
+++ app/presenters/PageableTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Components\Controls;
+
+trait PageableTrait
+{
+    /** @var int @persistent */
+    public $page = 1;
+
+    /** @var Controls\VisualPaginator */
+    protected $vp;
+
+    /**
+     * @return Controls\VisualPaginator
+     */
+    protected function createComponentVp()
+    {
+        return $this->vp;
+    }
+
+    /**
+     * @param int $itemCount
+     * @param int $limit
+     */
+    protected function preparePaginator($itemCount, $limit)
+    {
+        $this->vp = new Controls\VisualPaginator($this->page);
+        $p        = $this->vp->getPaginator();
+        $p->setItemCount($itemCount);
+        $p->setItemsPerPage($limit);
+        $p->setPage($this->page);
+    }
+}
