ee3b9f73e32f5fba50727ebcd6032e86f3fd837b
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-02-28 17:56:45 +0100

    Removed unnecessary condition from BaseEntity::setValues()

--- app/model/Entities/BaseEntity.php
+++ app/model/Entities/BaseEntity.php
@@ -5,6 +5,7 @@ namespace App\Model\Entities;
 use Doctrine\ORM\Mapping as ORM;
 use Kdyby;
 use Kdyby\Doctrine\Entities\Attributes\Identifier;
+use Nette;
 
 /**
  * @ORM\MappedSuperclass()
@@ -22,19 +23,11 @@ abstract class BaseEntity extends Kdyby\Doctrine\Entities\BaseEntity
         if ($values instanceof \Traversable) {
             $values = iterator_to_array($values);
         } elseif (!is_array($values)) {
-            throw new InvalidArgumentException(sprintf('Parameter must be an array, %s given.', gettype($values)));
+            throw new Nette\InvalidArgumentException(sprintf('Parameter must be an array, %s given.', gettype($values)));
         }
 
         foreach ($values as $key => $value) {
-            if ($key === 'languageContainer') {
-                foreach ($value as $locale => $data) {
-                    foreach ($data as $k => $v) {
-                        $this->translate($locale)->{$k} = $v !== '' ? $v : null;
-                    }
-                }
-            } else {
-                $this->{$key} = $value !== '' ? $value : null;
-            }
+            $this->{$key} = $value !== '' ? $value : null;
         }
     }
 }
