85952d4ea1eb8a08483fa33884e58a3aa298b5b2
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-03-12 12:48:00 +0100

    Cookies confirmation

--- www/js/main.js
+++ www/js/main.js
@@ -39,4 +39,19 @@ $(function() {
         });
     })();
 
+    (function() {
+        var cookieName = 'cshhCookiesConfirmation';
+
+        if (document.cookie.indexOf(cookieName) === -1) {
+            var $container = $('#cookies-confirmation');
+
+            $container[0].style.display = 'block';
+
+            $container.find('.confirmation-button').click(function() {
+                document.cookie = cookieName + '=' + true + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+                $container.fadeOut();
+            });
+        }
+    })();
+
 });
