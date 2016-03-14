15a9d84c0f90caca5ae98c57750fdb02077d1700
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-03-14 18:44:12 +0100

    CKEDITOR config

--- www/js/main.js
+++ www/js/main.js
@@ -11,8 +11,35 @@ $(function() {
     })();
 
     (function() {
-        CKEDITOR.config.customConfig = './ckeditor-config.js';
-        CKEDITOR.replaceAll('editor');
+        CKEDITOR.replaceAll(function(textarea, config) {
+            config.toolbarGroups = [
+                { name: 'styles', groups: [ 'styles' ] },
+                { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                { name: 'links', groups: [ 'links' ] },
+                { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+                { name: 'tools', groups: [ 'tools' ] },
+                { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                { name: 'insert', groups: [ 'insert' ] },
+                { name: 'forms', groups: [ 'forms' ] },
+                { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+                { name: 'others', groups: [ 'others' ] },
+                { name: 'colors', groups: [ 'colors' ] },
+                { name: 'about', groups: [ 'about' ] }
+            ];
+
+            config.removeButtons = 'Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Scayt,Anchor,Image,Table,HorizontalRule,SpecialChar,Source,Outdent,Indent,Blockquote,About,Styles';
+
+            config.removePlugins = 'elementspath';
+
+            config.resize_enabled = false;
+
+            config.format_tags = 'p;h2;h3;h4;h5;h6';
+
+            config.language = 'cs';
+
+            return $(textarea).hasClass('editor') ? true : false;
+        });
     })();
 
     $.nette.ext({
