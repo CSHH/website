241ec411ec8052ca205528d148619038cd48b628
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-03-14 17:51:10 +0100

    Split main.js into more files

--- www/js/main.js
+++ www/js/main.js
@@ -10,50 +10,6 @@ $(function() {
         });
     })();
 
-    (function() {
-        $('[data-modal-target]').click(function() {
-            var attr = $(this).attr('data-modal-target');
-            var $target = $('.modal').filter('[data-modal=' + attr + ']');
-            $('.modal').removeClass('active');
-            $('#black-filter').addClass('active');
-            $target.addClass('active');
-            $('body').css('overflow', 'hidden');
-
-            var windowHeight = window.innerHeight;
-            var targetHeight = $target.height();
-
-            if (windowHeight > targetHeight) {
-                var gap = (windowHeight - targetHeight) / 2;
-                $target[0].style.top = gap + 'px';
-            } else {
-                $target[0].style.top = '0px';
-                $target[0].style.height = windowHeight + 'px';
-                $target[0].style.overflow = 'auto';
-            }
-        });
-
-        $('.modal > .close').click(function() {
-            $('.modal').removeClass('active');
-            $('#black-filter').removeClass('active');
-            $('body').css('overflow', 'auto');
-        });
-    })();
-
-    (function() {
-        var cookieName = 'cshhCookiesConfirmation';
-
-        if (document.cookie.indexOf(cookieName) === -1) {
-            var $container = $('#cookies-confirmation');
-
-            $container[0].style.display = 'block';
-
-            $container.find('.confirmation-button').click(function() {
-                document.cookie = cookieName + '=' + true + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
-                $container.fadeOut();
-            });
-        }
-    })();
-
     $.nette.ext({
         load: function() {
             $('.flash, .form-error').find('.close').click(function() {
@@ -61,5 +17,7 @@ $(function() {
             });
         }
     });
+
+    $.nette.init();
 
 });
