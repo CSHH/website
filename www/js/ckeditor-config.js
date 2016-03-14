5a268e67df84ce45c819cabc64745c809c10a97b
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-03-14 18:47:27 +0100

    CKEDITOR config

--- www/js/ckeditor-config.js
+++ www/js/ckeditor-config.js
@@ -1,28 +1,31 @@
-CKEDITOR.editorConfig = function(config) {
+$(function() {
+    CKEDITOR.replaceAll(function(textarea, config) {
+        config.toolbarGroups = [
+            { name: 'styles', groups: [ 'styles' ] },
+            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+            { name: 'links', groups: [ 'links' ] },
+            { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+            { name: 'tools', groups: [ 'tools' ] },
+            { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+            { name: 'insert', groups: [ 'insert' ] },
+            { name: 'forms', groups: [ 'forms' ] },
+            { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+            { name: 'others', groups: [ 'others' ] },
+            { name: 'colors', groups: [ 'colors' ] },
+            { name: 'about', groups: [ 'about' ] }
+        ];
 
-	config.toolbarGroups = [
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
+        config.removeButtons = 'Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Scayt,Anchor,Image,Table,HorizontalRule,SpecialChar,Source,Outdent,Indent,Blockquote,About,Styles';
 
-	config.removeButtons = 'Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Scayt,Anchor,Image,Table,HorizontalRule,SpecialChar,Source,Outdent,Indent,Blockquote,About,Styles';
+        config.removePlugins = 'elementspath';
 
-	config.removePlugins = 'elementspath';
+        config.resize_enabled = false;
 
-	config.resize_enabled = false;
+        config.format_tags = 'p;h2;h3;h4;h5;h6';
 
-	config.format_tags = 'p;h2;h3;h4;h5;h6';
+        config.language = 'cs';
 
-	config.language = 'cs';
-};
+        return $(textarea).hasClass('editor') ? true : false;
+    });
+});
