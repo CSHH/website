a7c1a692ebca34e5954df70300a5b9e6cf968766
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-03-18 19:11:25 +0100

    Homepage - display news, latest articles, images and videos

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -2,6 +2,31 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\Model\Repositories;
+use App\Model\Videos\VideoThumbnail;
+
 final class HomepagePresenter extends BasePresenter
 {
+    /** @var Repositories\ArticleRepository @inject */
+    public $articleRepository;
+
+    /** @var Repositories\ImageRepository @inject */
+    public $imageRepository;
+
+    /** @var Repositories\VideoRepository @inject */
+    public $videoRepository;
+
+    public function renderDefault()
+    {
+        $parameters = $this->context->parameters;
+
+        $this->template->uploadDir = $parameters['uploadDir'];
+
+        $this->template->videoThumbnail = new VideoThumbnail($parameters['wwwDir'], $parameters['videoThumbnailsDir'], $parameters['vimeoOembedEndpoint']);
+
+        $this->template->news           = $this->articleRepository->getAllNews();
+        $this->template->latestArticles = $this->articleRepository->getLatestArticles();
+        $this->template->latestImages   = $this->imageRepository->getLatestImages();
+        $this->template->latestVideos   = $this->videoRepository->getLatestVideos();
+    }
 }
