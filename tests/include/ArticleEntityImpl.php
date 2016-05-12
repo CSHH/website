4c84f31e356f0367900f93d5aff695f643887632
Lukáš Neubauer <lukasneubauer@seznam.cz>
2016-05-12 18:09:14 +0200

    Tests - MenuCache and testing entities

---
+++ tests/include/ArticleEntityImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppTests;
+
+use App\Model\Entities\ArticleEntity;
+
+class ArticleEntityImpl extends ArticleEntity
+{
+    public $id;
+}
---
+++ tests/include/TagEntityImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppTests;
+
+use App\Model\Entities\TagEntity;
+
+class TagEntityImpl extends TagEntity
+{
+    public $id;
+}
