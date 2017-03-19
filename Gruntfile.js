74a6941d53b4f0a6a363680f379c2c67245c261a
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-03-19 21:24:17 +0100

    Assets and Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,74 +2,43 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         concat: {
-            dist: {
+            js: {
+                options: {
+                    separator: ';'
+                },
                 files: [
-                    {
-                        options: {
-                            separator: ';'
-                        },
-                        src: [
-                            'node_modules/jquery/dist/jquery.js',
-                            'node_modules/lightbox2/dist/js/lightbox.js',
-                            'node_modules/nette-forms/src/assets/netteForms.js',
-                            'node_modules/nette.ajax.js/nette.ajax.js',
-                            'assets/js/cookies.js',
-                            'assets/js/modals.js',
-                            'assets/js/themes.js',
-                            'assets/js/main.js'
-                        ],
-                        dest: 'www/js/main-front.js'
-                    }, {
-                        options: {
-                            separator: ';'
-                        },
-                        src: [
-                            'node_modules/ckeditor/ckeditor.js',
-                            'node_modules/jquery/dist/jquery.js',
-                            'node_modules/nette-forms/src/assets/netteForms.js',
-                            'node_modules/nette.ajax.js/nette.ajax.js',
-                            'assets/js/ckeditor-config.js',
-                            'assets/js/main.js'
-                        ],
-                        dest: 'www/js/main-admin.js'
-                    }, {
-                        src: [
-                            'node_modules/lightbox2/dist/css/lightbox.css',
-                            'assets/css/common.css',
-                            'assets/css/front.css',
-                            '!assets/css/theme-*.css'
-                        ],
-                        dest: 'www/css/main-front.css'
-                    }, {
-                        src: [
-                            'assets/css/common.css',
-                            'assets/css/admin.css'
-                        ],
-                        dest: 'www/css/main-admin.css'
-                    }
+                    {src: ['assets/js/{main,cookies,modals,themes}.js'], dest: 'www/js/main-front.js'},
+                    {src: ['assets/js/{main,ckeditor-config}.js'], dest: 'www/js/main-admin.js'}
+                ]
+            },
+
+            css: {
+                files: [
+                    {src: ['assets/css/{common,front}.css'], dest: 'www/css/main-front.css'},
+                    {src: ['assets/css/{common,admin}.css'], dest: 'www/css/main-admin.css'}
                 ]
             }
         },
+
         uglify: {
-            dist: {
-                files: {
-                    'www/js/main-front.min.js': ['<%= concat.dist.files[0].dest %>'],
-                    'www/js/main-admin.min.js': ['<%= concat.dist.files[1].dest %>']
-                }
-            }
+            files: [
+                {src: ['<%= concat.js.files[0].dest %>'], dest: 'www/js/main-front.min.js'},
+                {src: ['<%= concat.js.files[1].dest %>'], dest: 'www/js/main-admin.min.js'}
+            ]
         },
+
         cssmin: {
-            dist: {
-                files: {
-                    'www/css/main-front.min.css': ['<%= concat.dist.files[2].dest %>'],
-                    'www/css/main-admin.min.css': ['<%= concat.dist.files[3].dest %>'],
-                    'www/css/theme-fog.min.css': ['assets/css/theme-fog.css'],
-                    'www/css/theme-otherworld.min.css': ['assets/css/theme-otherworld.css']
-                }
-            }
+            files: [
+                {src: ['<%= concat.css.files[0].dest %>'], dest: 'www/css/main-front.min.css'},
+                {src: ['<%= concat.css.files[1].dest %>'], dest: 'www/css/main-admin.min.css'},
+                {src: ['assets/css/themes/theme-fog.css'], dest: 'www/css/theme-fog.min.css'},
+                {src: ['assets/css/themes/theme-otherworld.css'], dest: 'www/css/theme-otherworld.min.css'}
+            ]
         },
-        copy: {
+
+        /*copy: {
             lightboxImages: {
                 files: [
                     {
@@ -85,10 +54,12 @@ module.exports = function(grunt) {
                     }
                 ]
             }
-        },
+        },*/
+
         jshint: {
             files: ['Gruntfile.js', 'assets/js/*.js']
         },
+
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
@@ -98,9 +69,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'copy']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
 };
