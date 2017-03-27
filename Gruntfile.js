68cbf89c2226fb949c86a9a58e27ae9b2968d668
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-03-27 15:48:57 +0200

    Added chat.js in concat:js task in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
                     separator: ';\n\n\n'
                 },
                 files: [
-                    {src: ['assets/js/{main,ckeditor-config,cookies,modals,themes}.js'], dest: 'www/js/main-front.js'},
+                    {src: ['assets/js/{main,ckeditor-config,cookies,modals,themes,chat}.js'], dest: 'www/js/main-front.js'},
                     {src: ['assets/js/{main,ckeditor-config}.js'], dest: 'www/js/main-admin.js'},
                     {
                         src: [
