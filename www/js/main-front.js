98ce3d631e85556e194f8c253143572d3aa62d99
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-03-27 20:42:50 +0200

    Compiled and minified JS

--- www/js/main-front.js
+++ www/js/main-front.js
@@ -1,6 +1,22 @@
 $(function() {
+
+    var cookieName = 'cshhHideChat';
+    var ix = document.cookie.indexOf(cookieName);
+    var substr = document.cookie.substr(ix, cookieName.length + 2);
+    var arr = substr.split('=');
+
+    if (arr[1] === 't') {
+        $('#chatbox').removeClass('active');
+    }
+
     $('#chatbox .chatbox-switch').click(function() {
-        $('#chatbox').toggleClass('active');
+        if ($('#chatbox').hasClass('active')) {
+            $('#chatbox').removeClass('active');
+            document.cookie = cookieName + '=' + true + '; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+        } else {
+            $('#chatbox').addClass('active');
+            document.cookie = cookieName + '=' + false + '; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+        }
     });
 });
 ;
