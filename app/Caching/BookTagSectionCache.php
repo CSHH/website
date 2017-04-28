ec76cf47308fd736338394e57a125250ac3770dd
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-04-28 15:42:55 +0200

    Unit tests for book tag section cache

--- app/Caching/BookTagSectionCache.php
+++ app/Caching/BookTagSectionCache.php
@@ -27,6 +27,14 @@ class BookTagSectionCache implements TagSectionCacheInterface
     }
 
     /**
+     * @return Repositories\WikiRepository
+     */
+    public function getWikiRepository()
+    {
+        return $this->wikiRepository;
+    }
+
+    /**
      * @return array
      */
     public function getTags()
