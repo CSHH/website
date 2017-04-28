98530391a3af9184ea9a244af56896f577ceb840
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-04-28 15:53:11 +0200

    Unit tests for game tag section cache

--- app/Caching/GameTagSectionCache.php
+++ app/Caching/GameTagSectionCache.php
@@ -27,6 +27,14 @@ class GameTagSectionCache implements TagSectionCacheInterface
     }
 
     /**
+     * @return Repositories\WikiRepository
+     */
+    public function getWikiRepository()
+    {
+        return $this->wikiRepository;
+    }
+
+    /**
      * @return array
      */
     public function getTags()
