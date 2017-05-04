59d14e65c38a13426a3ab5b44353d1de82e656ef
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-05-04 21:34:00 +0200

    Fixed CS

--- fixtures/bootstrap.php
+++ fixtures/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-$container = require __DIR__ . '/../app/bootstrap.php';
+$container     = require __DIR__ . '/../app/bootstrap.php';
 $entityManager = $container->getByType('Kdyby\Doctrine\EntityManager');
 Nelmio\Alice\Fixtures::load(__DIR__ . '/fixtures.php', $entityManager);
