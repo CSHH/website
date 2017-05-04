27734c39321b8f586949ae47e1df9e4d9c12ba93
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-05-04 22:28:42 +0200

    Html purifier factory is using static method to create html purifier instance

--- app/Utils/HtmlPurifierFactory.php
+++ app/Utils/HtmlPurifierFactory.php
@@ -4,20 +4,14 @@ namespace App\Utils;
 
 class HtmlPurifierFactory
 {
-    /** @var \HTMLPurifier_Config */
-    private $config;
-
-    public function __construct()
-    {
-        $this->config = \HTMLPurifier_Config::createDefault();
-        $this->config->set('HTML.Allowed', 'p,h2,h3,h4,h5,h6,strong,em,u,strike,a[href],ul,ol,li,img[src|alt]');
-    }
-
     /**
      * @return \HTMLPurifier
      */
-    public function createHtmlPurifier()
+    public static function createHtmlPurifier()
     {
-        return new \HTMLPurifier($this->config);
+        $config = \HTMLPurifier_Config::createDefault();
+        $config->set('HTML.Allowed', 'p,h2,h3,h4,h5,h6,strong,em,u,strike,a[href],ul,ol,li,img[src|alt]');
+
+        return new \HTMLPurifier($config);
     }
 }
