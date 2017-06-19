cfd8dbb3db7f1f4aec90bf856b542b5ed2e9ad88
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-06-19 19:02:36 +0200

    Fixed access to not-existing content

--- app/Front/PageablePresenter.php
+++ app/Front/PageablePresenter.php
@@ -36,14 +36,20 @@ abstract class PageablePresenter extends BasePresenter
      */
     protected function getTag($tagSlug)
     {
-        return $tagSlug ? $this->tagRepository->getBySlug($tagSlug) : null;
+        $tag = $tagSlug ? $this->tagRepository->getBySlug($tagSlug) : null;
+
+        if ($tagSlug && $tag === null) {
+            $this->throw404();
+        }
+
+        return $tag;
     }
 
     /**
      * @param Entities\TagEntity $tag
      * @param string             $slug
      */
-    protected function throw404IfNoTagOrSlug(Entities\TagEntity $tag, $slug)
+    protected function throw404IfNoTagOrSlug(Entities\TagEntity $tag = null, $slug = null)
     {
         if (!$tag || !$slug) {
             $this->throw404();
@@ -56,7 +62,7 @@ abstract class PageablePresenter extends BasePresenter
      */
     protected function throw404IfNoItemsOnPage(Paginator $items, Entities\TagEntity $tag = null)
     {
-        if ($tag && !$items || $this->vp->page > $this->vp->getPaginator()->getLastPage()) {
+        if ($tag && $items->count() === 0 || $this->vp->page > $this->vp->getPaginator()->getLastPage()) {
             $this->throw404();
         }
     }
