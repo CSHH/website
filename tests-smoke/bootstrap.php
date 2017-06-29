0b3e9b999be961f5419f2e66176a70587c9bb15f
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-06-29 12:16:53 +0200

    Decoupled smoke tests from other tests

--- tests-smoke/bootstrap.php
+++ tests-smoke/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
@@ -11,8 +11,8 @@ define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
 @mkdir(TEMP_DIR . '/sessions');
 
 $params = [
-    'appDir' => __DIR__ . '/../../app',
-    'wwwDir' => __DIR__ . '/../../www',
+    'appDir' => __DIR__ . '/../app',
+    'wwwDir' => __DIR__ . '/../www',
 ];
 
 $configurator = new Nette\Configurator;
@@ -20,7 +20,7 @@ $configurator->setDebugMode(true);
 $configurator->enableDebugger(TEMP_DIR);
 $configurator->setTempDirectory(TEMP_DIR);
 $configurator->addParameters($params);
-$configurator->addConfig(__DIR__ . '/../../config/config.neon');
+$configurator->addConfig(__DIR__ . '/../config/config.neon');
 $configurator->addConfig(__DIR__ . '/db.neon');
 
 return $configurator->createContainer();
