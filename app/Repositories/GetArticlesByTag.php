dbe607015b62dc40405675e3efa620ecfbeafceb
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-06-30 16:01:11 +0200

    Added classes with extracted functionality to access items by tag

---
+++ app/Repositories/GetArticlesByTag.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Dao\SingleUserContentDao;
+use App\Entities;
+
+class GetArticlesByTag implements AccessibleByTagInterface
+{
+    /** @var SingleUserContentDao */
+    private $dataAccess;
+
+    public function __construct(SingleUserContentDao $dataAccess)
+    {
+        $this->dataAccess = $dataAccess;
+    }
+
+    /**
+     * @param  Entities\TagEntity $tag
+     * @return Entities\ArticleEntity[]
+     */
+    public function getAllByTag(Entities\TagEntity $tag)
+    {
+        return $this->dataAccess->getAllByTag(Entities\ArticleEntity::class, $tag);
+    }
+}
---
+++ app/Repositories/GetMoviesByTag.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Dao\WikiDao;
+use App\Entities;
+
+class GetMoviesByTag implements AccessibleByTagInterface
+{
+    /** @var WikiDao */
+    private $dataAccess;
+
+    public function __construct(WikiDao $dataAccess)
+    {
+        $this->dataAccess = $dataAccess;
+    }
+
+    /**
+     * @param  Entities\TagEntity $tag
+     * @return Entities\WikiEntity[]
+     */
+    public function getAllByTag(Entities\TagEntity $tag)
+    {
+        return $this->dataAccess->getAllByTag($tag, Entities\WikiEntity::TYPE_MOVIE);
+    }
+}
