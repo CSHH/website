d342ca861d652c08626aef4c8e91e8d2e2666634
Lukáš Neubauer <lukasneubauer@seznam.cz>
2017-07-05 23:25:44 +0200

    Removed unique constraint for new path in backlink entity

--- app/Entities/BacklinkEntity.php
+++ app/Entities/BacklinkEntity.php
@@ -18,7 +18,7 @@ class BacklinkEntity extends TimestampableEntity
     protected $oldPath;
 
     /**
-     * @ORM\Column(type="string", unique=true)
+     * @ORM\Column(type="string")
      *
      * @var string
      */
